218b2ba517ec45d2a08749adb08ab0a6fdf150dc
Likolaps <natan.brault@laposte.net>
2025-10-16 12:25:23 +0200

    feat added new pin layout + new eac proof mode

--- src/usb_descriptors.c
+++ src/usb_descriptors.c
@@ -27,6 +27,15 @@
 
 #include "tusb.h"
 
+// Tina added defines
+#if (CONTROLLER_SIDE == 1)
+#define CONTROLLER_NAME "P1 AC Deck IIDX"
+#define CONTROLLER_SERIAL "573572"
+#else
+#define CONTROLLER_NAME "P2 AC Deck IIDX"
+#define CONTROLLER_SERIAL "573573"
+#endif
+
 /* A combination of interfaces must have a unique product id, since PC will save
  * device driver after the first plug. Same VID/PID with different interface e.g
  * MSC (first), then CDC (later) will possibly cause system error on PC.
@@ -42,7 +51,7 @@
 //--------------------------------------------------------------------+
 // Device Descriptors
 //--------------------------------------------------------------------+
-tusb_desc_device_t const desc_device_joy = {
+tusb_desc_device_t desc_device_joy = {
     .bLength = sizeof(tusb_desc_device_t),
     .bDescriptorType = TUSB_DESC_DEVICE,
     .bcdUSB = 0x0200,
@@ -51,17 +60,17 @@ tusb_desc_device_t const desc_device_joy = {
     .bDeviceProtocol = 0x00,
     .bMaxPacketSize0 = CFG_TUD_ENDPOINT0_SIZE,
 
-    .idVendor = 0xCafe,
-    .idProduct = USB_PID,
-    .bcdDevice = 0x0100,
+    // We'll set these in the callback function
+    .idVendor = 0,
+    .idProduct = 0,
 
+    .bcdDevice = 0x0100,
     .iManufacturer = 0x01,
     .iProduct = 0x02,
     .iSerialNumber = 0x03,
-
     .bNumConfigurations = 0x01};
 
-tusb_desc_device_t const desc_device_key = {
+tusb_desc_device_t desc_device_key = {
     .bLength = sizeof(tusb_desc_device_t),
     .bDescriptorType = TUSB_DESC_DEVICE,
     .bcdUSB = 0x0200,
@@ -70,8 +79,13 @@ tusb_desc_device_t const desc_device_key = {
     .bDeviceProtocol = 0x00,
     .bMaxPacketSize0 = CFG_TUD_ENDPOINT0_SIZE,
 
-    .idVendor = 0xCafe,
+#if (!SPOOF_KONAMI_CONTROLLER)
+    .idVendor = CUSTOM_VID,
     .idProduct = USB_PID,
+#else
+    .idVendor = 0x1ccf,
+    .idProduct = 0x8048,
+#endif
     .bcdDevice = 0x0100,
 
     .iManufacturer = 0x01,
@@ -82,8 +96,23 @@ tusb_desc_device_t const desc_device_key = {
 
 // Invoked when received GET DEVICE DESCRIPTOR
 // Application return pointer to descriptor
-uint8_t const* tud_descriptor_device_cb(void) {
-  return (uint8_t const*)(joy_mode_check ? &desc_device_joy : &desc_device_key);
+uint8_t const *tud_descriptor_device_cb(void)
+{
+  // Get the appropriate descriptor based on mode
+  tusb_desc_device_t *desc = (tusb_desc_device_t *)(joy_mode_check ? &desc_device_joy : &desc_device_key);
+
+  if (SPOOF_KONAMI_CONTROLLER)
+  {
+    desc->idVendor = 0x1ccf;
+    desc->idProduct = 0x8048;
+  }
+  else
+  {
+    desc->idVendor = CUSTOM_VID;
+    desc->idProduct = USB_PID;
+  }
+
+  return (uint8_t const *)desc;
 }
 
 //--------------------------------------------------------------------+
@@ -92,19 +121,18 @@ uint8_t const* tud_descriptor_device_cb(void) {
 
 uint8_t const desc_hid_report_joy[] = {
     GAMECON_REPORT_DESC_JOYSTICK(HID_REPORT_ID(REPORT_ID_JOYSTICK)),
-    GAMECON_REPORT_DESC_LIGHTS(HID_REPORT_ID(REPORT_ID_LIGHTS))
-};
+    GAMECON_REPORT_DESC_LIGHTS(HID_REPORT_ID(REPORT_ID_LIGHTS))};
 
 uint8_t const desc_hid_report_key[] = {
     GAMECON_REPORT_DESC_LIGHTS(HID_REPORT_ID(REPORT_ID_LIGHTS)),
     GAMECON_REPORT_DESC_NKRO(HID_REPORT_ID(REPORT_ID_KEYBOARD)),
-    TUD_HID_REPORT_DESC_MOUSE(HID_REPORT_ID(REPORT_ID_MOUSE))
-};
+    TUD_HID_REPORT_DESC_MOUSE(HID_REPORT_ID(REPORT_ID_MOUSE))};
 
 // Invoked when received GET HID REPORT DESCRIPTOR
 // Application return pointer to descriptor
 // Descriptor contents must exist long enough for transfer to complete
-uint8_t const* tud_hid_descriptor_report_cb(uint8_t itf) {
+uint8_t const *tud_hid_descriptor_report_cb(uint8_t itf)
+{
   (void)itf;
   return (joy_mode_check ? desc_hid_report_joy : desc_hid_report_key);
 }
@@ -113,7 +141,11 @@ uint8_t const* tud_hid_descriptor_report_cb(uint8_t itf) {
 // Configuration Descriptor
 //--------------------------------------------------------------------+
 
-enum { ITF_NUM_HID, ITF_NUM_TOTAL };
+enum
+{
+  ITF_NUM_HID,
+  ITF_NUM_TOTAL
+};
 
 #define CONFIG_TOTAL_LEN (TUD_CONFIG_DESC_LEN + TUD_HID_DESC_LEN)
 
@@ -131,7 +163,6 @@ uint8_t const desc_configuration_joy[] = {
                        sizeof(desc_hid_report_joy), EPNUM_HID,
                        CFG_TUD_HID_EP_BUFSIZE, 1)};
 
-
 uint8_t const desc_configuration_key[] = {
     // Config number, interface count, string index, total length, attribute,
     // power in mA
@@ -147,8 +178,9 @@ uint8_t const desc_configuration_key[] = {
 // Invoked when received GET CONFIGURATION DESCRIPTOR
 // Application return pointer to descriptor
 // Descriptor contents must exist long enough for transfer to complete
-uint8_t const* tud_descriptor_configuration_cb(uint8_t index) {
-  (void)index;  // for multiple configurations
+uint8_t const *tud_descriptor_configuration_cb(uint8_t index)
+{
+  (void)index; // for multiple configurations
   return (joy_mode_check ? desc_configuration_joy : desc_configuration_key);
 }
 
@@ -157,11 +189,12 @@ uint8_t const* tud_descriptor_configuration_cb(uint8_t index) {
 //--------------------------------------------------------------------+
 
 // array of pointer to string descriptors
-char const* string_desc_arr[] = {
-    (const char[]){0x09, 0x04},  // 0: is supported language is English (0x0409)
-    "SpeedyPotato",              // 1: Manufacturer
-    "Pico Game Controller",      // 2: Product
-    "123456",                    // 3: Serials, should use chip ID
+char const *string_desc_arr[] = {
+    (const char[]){0x09, 0x04}, // 0: is supported language is English (0x0409)
+#if (!SPOOF_KONAMI_CONTROLLER)
+    "SpeedyPotato",    // 1: Manufacturer
+    CONTROLLER_NAME,   // 2: Product
+    CONTROLLER_SERIAL, // 3: Serials, should use chip ID
     "Button 1",
     "Button 2",
     "Button 3",
@@ -178,6 +211,23 @@ char const* string_desc_arr[] = {
     "Red 2",
     "Green 2",
     "Blue 2",
+#else
+    "Konami Amusement",                        // 1: Manufacturer
+    "beatmania IIDX controller premium model", // 2: Product
+    "123456",                                  // 3: Serials, should use chip ID
+    "Key 1",
+    "Key 2",
+    "Key 3",
+    "Key 4",
+    "Key 5",
+    "Key 6",
+    "Key 7",
+    "UNUSED GPIO 27",
+    "Key E1",
+    "Key E2",
+    "Key E3",
+    "Key E4",
+#endif
 };
 
 static uint16_t _desc_str[64];
@@ -185,29 +235,51 @@ static uint16_t _desc_str[64];
 // Invoked when received GET STRING DESCRIPTOR request
 // Application return pointer to descriptor, whose contents must exist long
 // enough for transfer to complete
-uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid) {
+uint16_t const *tud_descriptor_string_cb(uint8_t index, uint16_t langid)
+{
   (void)langid;
 
   uint8_t chr_count;
 
-  if (index == 0) {
+  if (index == 0)
+  {
     memcpy(&_desc_str[1], string_desc_arr[0], 2);
     chr_count = 1;
-  } else {
-    // Note: the 0xEE index string is a Microsoft OS 1.0 Descriptors.
-    // https://docs.microsoft.com/en-us/windows-hardware/drivers/usbcon/microsoft-defined-usb-descriptors
+  }
+  else
+  {
+    const char *str;
 
-    if (!(index < sizeof(string_desc_arr) / sizeof(string_desc_arr[0])))
-      return NULL;
-
-    const char* str = string_desc_arr[index];
-
+    // Set strings based on runtime mode
+    if (SPOOF_KONAMI_CONTROLLER)
+    {
+      if (index == 1)
+        str = "Konami Amusement";
+      else if (index == 2)
+        str = "beatmania IIDX controller premium model";
+      else if (index == 3)
+        str = "123456";
+      // ...add other specific strings
+      else if (index < sizeof(string_desc_arr) / sizeof(string_desc_arr[0]))
+        str = string_desc_arr[index];
+      else
+        return NULL;
+    }
+    else
+    {
+      if (index < sizeof(string_desc_arr) / sizeof(string_desc_arr[0]))
+        str = string_desc_arr[index];
+      else
+        return NULL;
+    }
     // Cap at max char
     chr_count = strlen(str);
-    if (chr_count > 63) chr_count = 63;
+    if (chr_count > 63)
+      chr_count = 63;
 
     // Convert ASCII string into UTF-16
-    for (uint8_t i = 0; i < chr_count; i++) {
+    for (uint8_t i = 0; i < chr_count; i++)
+    {
       _desc_str[1 + i] = str[i];
     }
   }
